8619beb8285bee505a9821b7feeb42a2f790b2ed
Roman <vzdrizhni@gmail.com>
2021-09-10 14:52:37 +0500

    implement decrease and increase functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.initialCount ?? 0,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: this.props.initialCount ?? 0
-        };
-    }
+  decrease = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
 
-    render() {
-        return (
-            <div>
-                <button>+</button>
-                <span>0</span>
-                <button>-</button>
-            </div>
-        )
-    }
+  increase = () => {this.setState(prevState => ({ count: prevState.count + 1}))};
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increase}>+</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.decrease}>-</button>
+      </div>
+    );
+  }
 }
